da6f9381c35ac2f47e01e0eb2c51a27ddd3de459
Althuwaynee <omar.faisel@gmail.com>
2026-02-12 18:44:44 +0000

    Auto-update: 12Feb1844

--- auto_pull_server.sh
+++ auto_pull_server.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # Navigate to the project directory
-cd /home/omar/Documents/Dust/iraqairquality
+cd /home/omar/Dust/iraqairquality
 
 # Add all changes
-git add .
+git pull origin main
+
+#git add data/*.json
 
 # Commit with a timestamp (e.g., "Auto-commit: 2026-02-06 16:45")
-git commit -m "Auto-update: $(date +'%d%b%H%M')"
+#git commit -m "Auto-update: $(date +'%d%b%H%M')"
 
 # Push to the main branch
-git push origin main
+#git push origin mainS
---
+++ auto_push_pc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Navigate to the project directory
+cd /home/omar/Documents/Dust/iraqairquality
+
+# Add all changes
+git add . #codes index.html css js codes
+
+
+# Commit with a timestamp (e.g., "Auto-commit: 2026-02-06 16:45")
+git commit -m "Auto-update: $(date +'%d%b%H%M')"
+
+# Push to the main branch
+git push origin main
